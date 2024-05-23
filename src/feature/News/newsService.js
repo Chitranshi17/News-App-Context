70aa9e29f61363aa28b96532a4b629111f600141
Chitranshi1~ <chitranshigourana@gmail.com>
2024-05-23 13:09:19 +0530

    UpdateCode

--- src/feature/News/newsService.js
+++ src/feature/News/newsService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchNews = async (topic) => {
   const response = await axios(
-    `https://newsapi.org/v2/everything?q=${topic}&from=2024-05-21&sortBy=publishedAt&apiKey=41a7404cd629478288b2753c202bc934
+    `https://newsapi.org/v2/everything?q=${topic}&from=2024-05-22&sortBy=publishedAt&apiKey=41a7404cd629478288b2753c202bc934
     `
   );
 
